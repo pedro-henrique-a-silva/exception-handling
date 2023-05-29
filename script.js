40cb8e2734ffb3e4afe261d477d898ea06760174
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-29 18:57:20 -0300

    Requisitos 2 e 3 concluidos

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const verificaInputs = (valor1, valor2) => {
   if (!valor1 || !valor2) {
-    throw new Error('Preencha os campos para realizar a soma')
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+};
+
+const verifcaIsNumber = (valor1, valor2) => {
+  if (Number.isNaN(Number(valor1)) || Number.isNaN(Number(valor2))) {
+    throw new Error('Informe dois números para realizar a soma');
   }
 };
 
@@ -9,14 +15,17 @@ function calculateSum() {
   const value2 = document.getElementById('value2').value;
 
   try {
-    verificaInputs(value1, value2)
+    verificaInputs(value1, value2);
+    verifcaIsNumber(value1, value2);
     const result = Number(value1) + Number(value2);
     console.log(result);
     return result;
   } catch (error) {
     return error.message;
+  } finally {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
   }
-
 }
 
 function displayResult(result) {
@@ -26,7 +35,7 @@ function displayResult(result) {
 const sum = () => {
   const result = calculateSum();
   displayResult(result);
-}
+};
 
 window.onload = () => {
   const button = document.getElementById('button');
