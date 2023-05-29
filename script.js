7d97793c57547ab0cfd13e663486a1f903fe3f7d
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-29 18:47:55 -0300

    Requisito 1 concluido

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const verificaInputs = (valor1, valor2) => {
+  if (!valor1 || !valor2) {
+    throw new Error('Preencha os campos para realizar a soma')
+  }
+};
+
 function calculateSum() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
 
-  return result;
+  try {
+    verificaInputs(value1, value2)
+    const result = Number(value1) + Number(value2);
+    console.log(result);
+    return result;
+  } catch (error) {
+    return error.message;
+  }
+
 }
 
 function displayResult(result) {
